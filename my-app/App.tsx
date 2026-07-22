acae6d1c2efc275f2c4db36b5686f30274a7390b
ST10512443 <ST10512443@myemeris.edu.za>
2026-07-22 16:49:55 +0200

    Created app

---
+++ my-app/App.tsx
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+
+      <Text>Welcome to my app</Text>
+
+      <Text>Enter your Name</Text>
+      <TextInput placeholder="Jane"/>
+      <Text>Enter your Surname</Text>
+      <TextInput placeholder="Doe"/>
+
+
+      <Button title="Add User"/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
